1fe1c064a15e67b3ac1fa9c0bb05ab144d9fa86b
ironclad00 <ironclad00@gmail.com>
2014-08-04 23:05:37 +0000

    15 and 17 updates

---
+++ 15.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TARGET 2
+
+void transverse(int ***gridArray, int i, int j, double *numPaths);
+
+int main() {
+	int i,j;
+    double numPaths=0;
+    int **gridArray;					//0: down only, 1: right only, 2: both, 3: end of grid
+    
+    //initialize grid
+    gridArray = malloc((TARGET+1)*sizeof(int *));
+    for(i=0;i<(TARGET+1);i++) {
+        gridArray[i] = malloc((TARGET+1)*sizeof(int));
+    }
+    for(i=0;i<(TARGET+1);i++) {
+        for(j=0;j<(TARGET+1);j++) {
+            if(i==TARGET) {				//initializing the bottom of grid
+                gridArray[i][j]=1;
+            }
+            else if(j==TARGET) {		//initializing the right of grid
+                gridArray[i][j]=0;
+            }
+            else {
+            	gridArray[i][j]=2;		//initializing all other points on the grid
+            }
+        }
+    }
+    gridArray[TARGET][TARGET]=3;		//initializing the bottom-right corner
+    
+    transverse(&gridArray,0,0,&numPaths);
+    printf("There are a total of %.0f paths in a %dx%d grid.\n",numPaths,TARGET,TARGET);
+
+    return 0;
+}
+
+void  transverse(int ***gridArray, int i, int j, double *numPaths) {
+    if((*gridArray)[i][j]==0) {
+        transverse(gridArray,(i+1),j,numPaths);	//traverse down only
+    }
+    else if((*gridArray)[i][j]==1) {
+        transverse(gridArray,i,(j+1),numPaths);	//traverse right only
+    }
+    else if((*gridArray)[i][j]==2) {
+        transverse(gridArray,i,(j+1),numPaths);	//traverse both right
+        transverse(gridArray,(i+1),j,numPaths);	//and down
+    }
+    else {
+        *numPaths+=1.0;							//if end is reached, increase path count
+    }
+}
--- 17.c
+++ 17.c
@@ -1,66 +1,110 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-    int i;
+    int i,tens;
     long sum=0;
     int array[91];
+    char **testArray;
+    
+    //initialize array and testArray, including indexes which will only be accessed if an error occurs
     for(i=0;i<91;i++) {
     	array[i]=-30000;
     }
-    array[0]=0;
-    array[1]=3;
-    array[2]=3;
-    array[3]=5;
-    array[4]=4;
-    array[5]=4;
-    array[6]=3;
-    array[7]=5;
-    array[8]=5;
-    array[9]=4;
-    array[10]=3;
-    array[11]=6;
-    array[12]=6;
-    array[13]=8;
-    array[14]=8;
-    array[15]=7;
-    array[16]=7;
-    array[17]=9;
-    array[18]=8;
-    array[19]=8;
-    array[20]=6;
-    array[30]=6;
-    array[40]=5;
-    array[50]=5;
-    array[60]=5;
-    array[70]=7;
-    array[80]=6;
-    array[90]=6;
+    array[0]=0;		//0 letters
+    array[1]=3;		//"one"
+    array[2]=3;		//"two"
+    array[3]=5;		//"three"
+    array[4]=4;		//"four"
+    array[5]=4;		//"five"
+    array[6]=3;		//"six"
+    array[7]=5;		//"seven"
+    array[8]=5;		//"eight"
+    array[9]=4;		//"nine"
+    array[10]=3;	//"ten"
+    array[11]=6;	//"eleven"
+    array[12]=6;	//"twelve"
+    array[13]=8;	//"thirteen"
+    array[14]=8;	//"fourteen"
+    array[15]=7;	//"fifteen"
+    array[16]=7;	//"sixteen"
+    array[17]=9;	//"seventeen"
+    array[18]=8;	//"eighteen"
+    array[19]=8;	//"nineteen"
+    array[20]=6;	//"twenty"
+    array[30]=6;	//"thirty"
+    array[40]=5;	//"forty"
+    array[50]=5;	//"fifty"
+    array[60]=5;	//"sixty"
+    array[70]=7;	//"seventy"
+    array[80]=6;	//"eighty"
+    array[90]=6;	//"ninety"
+    
+    testArray = malloc(91*sizeof(int *));
+    for(i=0;i<91;i++) {
+        testArray[i] = malloc(array[i]*sizeof(int));
+    }
+    testArray[1]="one";
+    testArray[2]="two";
+    testArray[3]="three";
+    testArray[4]="four";
+    testArray[5]="five";
+    testArray[6]="six";
+    testArray[7]="seven";
+    testArray[8]="eight";
+    testArray[9]="nine";
+    testArray[10]="ten";
+    testArray[11]="eleven";
+    testArray[12]="twelve";
+    testArray[13]="thirteen";
+    testArray[14]="fourteen";
+    testArray[15]="fifteen";
+    testArray[16]="sixteen";
+    testArray[17]="seventeen";
+    testArray[18]="eighteen";
+    testArray[19]="nineteen";
+    testArray[20]="twenty";
+    testArray[30]="thirty";
+    testArray[40]="forty";
+    testArray[50]="fifty";
+    testArray[60]="sixty";
+    testArray[70]="seventy";
+    testArray[80]="eighty";
+    testArray[90]="ninety";
     
     for(i=1;i<1000;i++) {
-        int temp;
         if(i>=100) {
-            sum+=7;						 		//"hundred"
-            sum+=array[(int)(i/100)]; 	 		//how many hundreds
-	        temp=i-100*(int)(i/100);			//tens part of the number
-            if(!(i%100==0)) {
-                sum+=3;						 	//"and"
-                if(temp<=20 || temp%10==0) {
-                    sum+=array[temp];
+            sum+=7;						 			//"hundred"
+            sum+=array[(int)(i/100)]; 	 			//how many hundreds
+	        tens=i-100*(int)(i/100);				//tens part of the number
+            printf("%s hundred ",testArray[(int)(i/100)]);
+            if(!(i%100==0)) {						//if the number is not a multiple of 100
+                sum+=3;						 		//"and"
+                printf("and ");
+                if(tens<=20 || tens%10==0) {
+                    sum+=array[tens];
+                    printf("%s",testArray[tens]);
                 } else {
-                    sum+=array[(int)(temp/10)]; //tens
-                    sum+=array[temp%10];		//units
+                    sum+=array[(int)(tens/10)*10];	//tens
+                    sum+=array[tens%10];			//units
+                    printf("%s-%s",testArray[(int)(tens/10)*10],testArray[tens%10]);
                 }
             }
-        } else {
+        } else { //for all numbers less than 100
             if(i<=20 || i%10==0) {
                 sum+=array[i];
+                printf("%s",testArray[i]);
             } else {
-                sum+=array[(int)(i/10)]; 		//tens
-                sum+=array[i%10];				//units
+                sum+=array[(int)(i/10)*10]; 		//tens
+                sum+=array[i%10];					//units
+                printf("%s-%s",testArray[(int)(i/10)*10],testArray[i%10]);
             }
         }
+        printf("\n");
     }
-    sum+=11;									//"one thousand"
+    sum+=11;										//"one thousand"
+    printf("one thousand\n");
     
     printf("The sum of the number of letters in the numbers [1,1000] is %d.\n",sum);
 
