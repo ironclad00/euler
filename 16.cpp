0337e6f815b16bb8181950acf2feb4ebb6abd48e
ironclad00 <ironclad00@gmail.com>
2014-08-04 05:52:23 +0000

    Added 16 and its i/o file

--- 16.cpp
+++ 16.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <iomanip>
 using namespace std;
 
 int main() {
-	int i;
+	int i,bigLength;
     int sum=0;
     double big=2.0;
-    
+
     for(i=2;i<=1000;i++) {
         big*=2;
     }
-    
-    ifstream inputFile;
+
+    ofstream inputFile;
     inputFile.open("16io.txt");
-    if(inputFile.is_open())
+    if(inputFile.is_open()) {
+        inputFile << fixed << showpoint;  //this line and the next prevent 'big' from
+        inputFile << setprecision(1);     //being written out in scientific notation
         inputFile << big;
+    }
     else
         printf("A file-related error occurred.\n");
     inputFile.close();
-    
+
     string number;
-    ofstream outputFile;
+    ifstream outputFile;
     outputFile.open("16io.txt");
     if(outputFile.is_open())
         getline(outputFile,number);
     else
         printf("A file-related error occurred.\n");
     outputFile.close();
-    
-    for(i=0;i<string.length();i++) {
+
+    bigLength=number.length()-2;		//the last two items in the array are '.0'
+    for(i=0;i<bigLength;i++) {
     	sum+=(int)number[i]-48;
     }
-    
+
     printf("The sum of the digits that compose the number 2^1000 is %d.\n",sum);
 
     return 0;
